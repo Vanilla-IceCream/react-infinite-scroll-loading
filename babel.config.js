e09c2e9d94b4ea449883016410c42547b9c01189
Shyam Chen <shyamchen1994@gmail.com>
2019-09-02 11:16:48 +0800

    Initial Commit

---
+++ babel.config.js
@@ -0,0 +1,38 @@
+module.exports = api => {
+  api.cache(true);
+
+  return {
+    env: {
+      app: {
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              useBuiltIns: 'entry',
+              corejs: '3',
+              shippedProposals: true,
+              targets: '> 0.25%, not dead',
+            },
+          ]
+        ],
+        plugins: [],
+      },
+      test: {
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              useBuiltIns: 'entry',
+              corejs: '3',
+              shippedProposals: true,
+              targets: {
+                node: 'current',
+              },
+            },
+          ]
+        ],
+        plugins: [],
+      },
+    },
+  };
+};
